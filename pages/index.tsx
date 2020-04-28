3fd0c4cd4b12beb635e1c2a42f2a388e1867dd95
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-04-28 09:51:08 +0100

    Initial

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import * as React from 'react';
+
+interface IHomeProps {
+
+}
+
+const Home: React.FC<IHomeProps> = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+};
+
+export default Home;
